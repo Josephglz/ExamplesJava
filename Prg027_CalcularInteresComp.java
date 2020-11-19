413356b29849622231bc158d3a6824c0c699caeb
Joseph Ian Gonzalez <joseph220199@gmail.com>
2020-11-19 11:05:18 -0600

    Fix Program

--- Prg027_CalcularInteresComp.java
+++ Prg027_CalcularInteresComp.java
@@ -1,13 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.Scanner;
 
-/**
- *
- * @author josep
- */
-public class Prg027_CalcularInteresComp {
+public class Prg027_CalcularInteresComp 
+{
+    public static void main(String[] args)
+    {
+        Scanner read = new Scanner(System.in);
+        
+        Prg001_DesplEncabezado Display = new Prg001_DesplEncabezado();
+        Display.Encabezado("||  Calcular Interés Compuesto   ||", "||      Prg027_CalcularInteresComp      ||", "15/10/2020");
     
+    
+        float cap = 0.0f, inte = 0.0f, time  = 0.0f;
+        double monto = 0.0f;
+        
+        System.out.println("Ingrese el Monto: ");
+        cap = read.nextFloat();
+        System.out.println("Ingrese el Interés: ");
+        inte = read.nextFloat();
+        System.out.println("Ingrese los Años: ");
+        time = read.nextFloat();
+        
+        monto = (cap * (1 + (inte / 100)));
+        double res = Math.pow(monto, time);
+        
+        System.out.println("");
+	System.out.println("============== RESULTADO ==============");
+	System.out.println("       Calcular Interes Compuesto");
+	System.out.println("");
+	System.out.println("Tiempo: " + time + " años");
+	System.out.println("Interés: " + inte + " %");
+	System.out.println("Capital: $" + cap);
+	System.out.println("Interes Compuesto: $" + monto);
+	System.out.println("");
+	System.out.println("=======================================");
+    }
 }
